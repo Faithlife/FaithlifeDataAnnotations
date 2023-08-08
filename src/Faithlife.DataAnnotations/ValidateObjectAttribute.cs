f2bfa57cbfc6d02d878d8915f62f7cdc749add1b
Ed Ball <ed.ball@faithlife.com>
2023-08-08 09:03:11 -0700

    Use file-scoped namespaces.

--- src/Faithlife.DataAnnotations/ValidateObjectAttribute.cs
+++ src/Faithlife.DataAnnotations/ValidateObjectAttribute.cs
@@ -1,27 +1,26 @@
 using System.ComponentModel.DataAnnotations;
 
-namespace Faithlife.DataAnnotations
+namespace Faithlife.DataAnnotations;
+
+/// <summary>
+/// Used to validate a property whose type has its own properties with data annotations.
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public sealed class ValidateObjectAttribute : ValidationAttribute
 {
-	/// <summary>
-	/// Used to validate a property whose type has its own properties with data annotations.
-	/// </summary>
-	[AttributeUsage(AttributeTargets.Property)]
-	public sealed class ValidateObjectAttribute : ValidationAttribute
+	/// <inheritdoc />
+	protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
 	{
-		/// <inheritdoc />
-		protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
-		{
-			if (value is null)
-				return null;
+		if (value is null)
+			return null;
 
-			var validationResults = ValidatorUtility.GetValidationResults(value);
-			if (validationResults.Count == 0)
-				return null;
+		var validationResults = ValidatorUtility.GetValidationResults(value);
+		if (validationResults.Count == 0)
+			return null;
 
-			var innerErrorMessage = string.Join(" ", validationResults.Select(x => x.ErrorMessage).Where(x => x is not null));
-			return new ValidationResult(
-				errorMessage: $"{FormatErrorMessage(validationContext.DisplayName)}{(innerErrorMessage.Length == 0 ? "" : $" ({innerErrorMessage})")}",
-				memberNames: validationContext.MemberName is string memberName ? new[] { memberName } : null);
-		}
+		var innerErrorMessage = string.Join(" ", validationResults.Select(x => x.ErrorMessage).Where(x => x is not null));
+		return new ValidationResult(
+			errorMessage: $"{FormatErrorMessage(validationContext.DisplayName)}{(innerErrorMessage.Length == 0 ? "" : $" ({innerErrorMessage})")}",
+			memberNames: validationContext.MemberName is string memberName ? new[] { memberName } : null);
 	}
 }
